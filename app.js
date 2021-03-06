73384189c6ea4c18c636e80f7c1a4a2edeb0837f
Amarah Abusohyon <amarhbishtawi@gmail.com>
2021-03-06 22:26:51 +0200

    5th-problem

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -105,8 +105,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    let array = [multiply(multArr[0], multiply(multArr[1], multArr[2])[0])[0], 'The numbers 2,3,4 have a product of 24.']
+    return array;
 }
+
+// Here is the test for multiplyArray(); uncomment it to run it
+testMultiplyArray(testArray);
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
